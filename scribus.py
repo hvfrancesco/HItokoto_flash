03a1fe8548186de5a8092e7de9ef49f2c3849530
Francesco Fantoni <francesco@hv-a.com>
2017-07-30 18:13:51 +0200

    aggiunto primo test con scribus, usa l'api REST

---
+++ scribus.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+try:
+    # Please do not use 'from scribus import *' . If you must use a 'from import',
+    # Do so _after_ the 'import scribus' and only import the names you need, such
+    # as commonly used constants.
+    import scribus
+except ImportError,err:
+    print "This Python script is written for the Scribus scripting interface."
+    print "It can only be run from within Scribus."
+    sys.exit(1)
+
+import requests
+import json
+
+
+
+def main(argv):
+    """This is a documentation string. Write a description of what your code
+    does here. You should generally put documentation strings ("docstrings")
+    on all your Python functions."""
+    url = "http://localhost:5000/api/storia"
+    headers = {'Accept': 'application/vnd.api+json'}
+    post_headers = {'Accept': 'application/vnd.api+json', 'Content-Type': 'application/vnd.api+json'}
+    response = requests.get(url, headers=headers)
+    assert response.status_code == 200
+    risposta = response.json()
+    oggetti = risposta['objects']
+
+    userdim = scribus.getUnit()
+    scribus.setUnit(scribus.UNIT_MM)
+    scribus.progressTotal(len(oggetti))
+    scribus.setRedraw(False)
+
+    nome_x = 60
+    nome_y = 45
+    nome_w = 74
+    nome_h = 10
+
+    testo_x = 14
+    testo_y = 62
+    testo_w = 120
+    testo_h = 133
+
+    i=0
+
+
+    storie = []
+    for o in oggetti:
+        b = o['body']
+        a = o['autore']['nome']
+        t = o['timestamp']
+        s = {'autore':a, 'timestamp':t, 'testo':b}
+        storie.append(s)
+        scribus.newPage(-1)
+        textbox = scribus.createText(nome_x, nome_y, nome_w, nome_h)
+        scribus.insertText(o['autore']['nome'],0,textbox)
+        scribus.setTextColor('Black',textbox)
+        scribus.setFont('Nimbus Sans L Bold', textbox)
+        scribus.setFontSize(14, textbox)
+        scribus.setLineSpacing(16, textbox)
+        textbox = scribus.createText(testo_x, testo_y, testo_w, testo_h)
+        scribus.insertText(o['body'],0,textbox)
+        scribus.setTextColor('Black',textbox)
+        scribus.setFont('Apple Garamond Regular', textbox)
+        scribus.setFontSize(11, textbox)
+        scribus.setLineSpacing(14, textbox)
+        scribus.newPage(-1)
+        i += 1
+        scribus.progressSet(i)
+
+    scribus.progressReset()
+    scribus.setUnit(userdim)
+    scribus.docChanged(True)
+    scribus.statusMessage("fatto")
+    scribus.setRedraw(True)
+    print storie
+
+def main_wrapper(argv):
+    """The main_wrapper() function disables redrawing, sets a sensible generic
+    status bar message, and optionally sets up the progress bar. It then runs
+    the main() function. Once everything finishes it cleans up after the main()
+    function, making sure everything is sane before the script terminates."""
+    try:
+        scribus.statusMessage("Running script...")
+        scribus.progressReset()
+        main(argv)
+    finally:
+        # Exit neatly even if the script terminated with an exception,
+        # so we leave the progress bar and status bar blank and make sure
+        # drawing is enabled.
+        if scribus.haveDoc():
+            scribus.setRedraw(True)
+        scribus.statusMessage("")
+        scribus.progressReset()
+
+# This code detects if the script is being run as a script, or imported as a module.
+# It only runs main() if being run as a script. This permits you to import your script
+# and control it manually for debugging.
+if __name__ == '__main__':
+    main_wrapper(sys.argv)
